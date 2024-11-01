efa945c37068b76ff2ca5b937e70cd600ddb8d18
apalskala <vladmanole6@gmail.com>
2024-11-01 09:01:44 +0200

    test fisier dat

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public static void main(String[] args){
     //TasksView.GUI();
 
 
-    String folderPath = "C:\\Users\\vladm\\OneDrive\\Desktop\\folder proiect"; // Specify your folder path here
+    //String folderPath = "C:\\Users\\vladm\\OneDrive\\Desktop\\folder proiect"; // Specify your folder path here
+    String folderPath = "C:\\Users\\vladm\\OneDrive\\Desktop\\facultateig\\OOP\\proiect 3\\House-manager\\src\\testItemFolder"; // Specify your folder path here
+
     AppInitializer appInitializer = new AppInitializer(folderPath);
     appInitializer.launchUI();
 
