12dedb6a6b01b88fc150ebba3f843342d9b7937d
apalskala <vladmanole6@gmail.com>
2024-11-01 12:43:50 +0200

    0.01 version doamne ajuta sa luam nota

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public static void main(String[] args){
 
 
     //String folderPath = "C:\\Users\\vladm\\OneDrive\\Desktop\\folder proiect"; // Specify your folder path here
+    //asdasd fsdvsd
     String folderPath = "C:\\Users\\vladm\\OneDrive\\Desktop\\facultateig\\OOP\\proiect 3\\House-manager\\src\\testItemFolder"; // Specify your folder path here
 
     AppInitializer appInitializer = new AppInitializer(folderPath);
